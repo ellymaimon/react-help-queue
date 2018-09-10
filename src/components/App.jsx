c27b1a810b90f447eb7933894982de2b0b3f2e97
ellymaimon <eliranmaimon@gmail.com>
2018-09-09 22:34:48 -0700

    add handle method, bind it, and add render props to the route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,15 +12,24 @@ class App extends React.Component {
     this.state = {
       masterTicketList: []
     };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this)
   }
-   
+
+  handleAddingNewTicketToList(newTicket) {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
   render(){
     return(
       <div>
         <Header/>
         <Switch>
-          <Route exact path='/' component={TicketList} />
-          <Route path='/newticket' component={NewTicketControl} />
+          <Route exact path='/'
+                 render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket'
+                 render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
           <Route component={Error404} />
         </Switch>
       </div>
